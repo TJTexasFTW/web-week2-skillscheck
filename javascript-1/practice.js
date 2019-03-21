f5d2b14e03f5e2c124ccaf37a46e84f407cabc1e
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-03-21 09:40:43 -0500

    Through Problem 11

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -3,42 +3,62 @@
 // Create a variable called 'myName' that's value is your name
 
 // Code here
+let myName = "TJ";
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' that's value is your favorite number
 
 //Code here
+let faveNum = 13;
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and set it to true
 
 //Code here
-
+let lovesCode = true;
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, num1 and num2, and returns the sum of those two parameters.
 
 //Code here
-
+function sum(num1, num2) {
+	return num1 + num2;
+}
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called x. Check to see if x is equal to true. If it is, return true as a boolean. If x does not equal true, return false as a boolean
 
 //Code here
+function lovesCodeChecker(x){
+	if (x) {
+		return true;
+	} return false;
+}
 
 //////////////////PROBLEM 6////////////////////
 
 // Create a function called 'oddChecker' that takes in one parameter, num. Check to see if the number is even or odd. If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
 
 //Code here
+function oddChecker(num) {
+	if (num %2 === 0) {
+		return "the number is even";
+	} return "the number is odd";
+}
+
 
 //////////////////PROBLEM 7////////////////////
 
 // Create a function called 'iLove' that takes in two parameters, name which will be a string  and love which will be a string of something you love. Have the function take the two parameters and retrun a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
 
 //Code Here
+function iLove(name, love) {
+	return name + " loves " + love;
+}
+
+console.log(iLove("TJ", "my dog"));
 
 //////////////////PROBLEM 8////////////////////
 
@@ -46,12 +66,13 @@
 const faveColors = ['red', 'green', 'black']
 
 //Code Here
-
+let colorCopy = [...faveColors];
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push
 
 //Code Here
+colorCopy.push("red");
 
 //////////////////PROBLEM 10////////////////////
 
@@ -59,12 +80,22 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //Code Here
+let middleNums = [...numbers];
+middleNums.shift();
+middleNums.pop();
 
 //////////////////PROBLEM 11////////////////////
 
 // Create an object called 'me' that has these keys with the correct values: firstName, state, age, greeter. firstName will be your name as a string. state will be your current state or providence as a string. age will be your age as a number. greeter will be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the correct values
 
 //Code Here
+let me = {
+	firstName: "TJ", 
+	state: "TX", 
+	age: 21, 
+	greeter: function(){
+	return "Hello! My name is " + this.firstName + " and I live in " + this.state;
+  }};
 
 //////////////////PROBLEM 12////////////////////
 
